15e20a4b4e8f9a437fbaf04b2029bd898d7236c9
José Juan Martínez Peláez <josejuan@mpz.mx>
2022-06-13 09:13:19 -0500

    Ajustes para PHP 8

--- src/API.php
+++ src/API.php
@@ -10,19 +10,19 @@ use \Mpsoft\STM\Sesion\Token;
 
 function STM_GET_Elemento(string $elemento_clase, ?int $elemento_id = NULL, ?callable $preparar_elemento_inicializado = NULL, ?callable $obtener_resultado=NULL):array
 {
-    return FDW_GET_Elemento($elemento_clase,  $elemento_id, STM_Bloquear_Elemento, STM_Obtener_Resultado);
+    return FDW_GET_Elemento($elemento_clase,  $elemento_id, "STM_Bloquear_Elemento", "STM_Obtener_Resultado");
 }
 
 function STM_POST_Elemento(array $OPENAPI_REQUEST, string $elemento_clase, ?int $elemento_id = NULL, ?callable $guardar_informacion_adicional = NULL, ?callable $procesar_exception_aplicarcambios=NULL, &$elemento = NULL):array
 {
     $campos_a_ignorar = array("activo", "bloqueo_tiempo", "bloqueo_token_id", "creacion", "modificacion");
 
-    return FDW_POST_Elemento($OPENAPI_REQUEST, $elemento_clase, $elemento_id, STM_Verificar_Elemento_Inicializado, $campos_a_ignorar, $guardar_informacion_adicional, STM_Obtener_Resultado, $procesar_exception_aplicarcambios, $elemento);
+    return FDW_POST_Elemento($OPENAPI_REQUEST, $elemento_clase, $elemento_id, "STM_Verificar_Elemento_Inicializado", $campos_a_ignorar, $guardar_informacion_adicional, "STM_Obtener_Resultado", $procesar_exception_aplicarcambios, $elemento);
 }
 
 function STM_DELETE_Elemento(string $elemento_clase, ?int $elemento_id = NULL, ?callable $preparar_elemento_inicializado = NULL):array
 {
-    return FDW_DELETE_Elemento($elemento_clase, $elemento_id, STM_Puede_Eliminar);
+    return FDW_DELETE_Elemento($elemento_clase, $elemento_id, "STM_Puede_Eliminar");
 }
 
 function STM_Verificar_Elemento_Inicializado($elemento):?array
